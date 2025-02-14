801d30e5b57457fe5a2b6f7bf8e229a22439331b
Abhinav Anand <iabhinavanand10@gmail.com>
2025-02-14 12:37:26 +0530

    Initial Commit

---
+++ threadsTuT/threadsTuTApp.swift
@@ -0,0 +1,17 @@
+//
+//  threadsTuTApp.swift
+//  threadsTuT
+//
+//  Created by ABHINAV ANAND  on 14/02/25.
+//
+
+import SwiftUI
+
+@main
+struct threadsTuTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
